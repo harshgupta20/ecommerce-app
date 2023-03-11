7ddb3bf3389eb12a96e85cb07f63eae89dc3f43d
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-03-12 02:19:33 +0530

    Admin panel completed (Only update feature remaining)

--- src/config/HandlingCalls.js
+++ src/config/HandlingCalls.js
@@ -1,23 +1,33 @@
-import {addDoc, getDocs, collection, setDoc, doc } from "firebase/firestore";
-import {db} from "./Firebase";
+import { getDocs, collection, setDoc, doc, deleteDoc } from "firebase/firestore";
+import { getStorage, ref, deleteObject } from "firebase/storage";
+import { db, storage } from "./Firebase";
 
 const productRef = collection(db, "products");
 
 export const productRead = async () => {
     const data = await getDocs(productRef);
     // const data.docs.map((doc) => ({...doc.data(), id : doc.id})));
-    return data.docs.map((doc) => ({...doc.data(), id : doc.id}));
+    return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 }
 
 export const productAdd = async (id, name, category, amount, imageURL) => {
     console.log("Strat");
     const path = id;
-    await setDoc(doc(db, "products", path),{
-        product_name:"dsad",
-        product_image:imageURL,
-        product_category:"nsdf",
-        product_amount: "sdfnm"
+    await setDoc(doc(db, "products", path), {
+        product_name: name,
+        product_image: imageURL,
+        product_category: category,
+        product_amount: amount
     });
 
     console.log("End");
+}
+
+
+
+export const productDelete = async (product_id) => {
+    await deleteDoc(doc(db, "products", product_id));
+    const desertRef = ref(storage, `product_images/${product_id}`);
+    // Delete the file
+    deleteObject(desertRef);
 }
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -6,8 +6,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { ref, uploadBytes } from 'firebase/storage';
 import { getDownloadURL } from "firebase/storage";
 
-import { productRead, productAdd } from "../config/HandlingCalls";
-import { sha256 } from 'crypto-hash';
+import { productRead, productAdd, productDelete } from "../config/HandlingCalls";
 
 const Admin = () => {
 
@@ -18,8 +17,10 @@ const Admin = () => {
   const [name, setName] = useState('')
   const [category, setCategory] = useState('')
   const [amount, setAmount] = useState('')
-  const [imagePath, setImagePath] = useState('')
+  const [imagePath, setImagePath] = useState(null)
   const [imageURL, setImageURL] = useState();
+
+  const [checkUploadStatus, setCheckUploadStatus] = useState(false);
 
 
 
@@ -33,34 +34,59 @@ const Admin = () => {
   // Uploading the image and getting the url
   const uploadImagehandle = async () => {
     console.log("inside");
-    if(id){
-    console.log("id done");
+    if (id && imagePath) {
+      console.log("path");
+      console.log(imagePath);
       const uploadRef = ref(storage, `product_images/${id}`);
-        await uploadBytes(uploadRef, imagePath).then(() => {
-          console.log(id);
-          alert("Uploaded Success !");
+      await uploadBytes(uploadRef, imagePath).then(() => {
+        console.log(id);
+        alert("Uploaded Success !");
+      })
+      // Download Link
+      await getDownloadURL(uploadRef)
+        .then((url) => {
+          setImageURL(url);
+          console.log("Image URL");
+          console.log(url);
         })
-        // Download Link
-        await getDownloadURL(uploadRef)
-          .then((url) => {
-            // `url` is the download URL for 'images/stars.jpg'
-            // setImageURL(url);
-            setImageURL(url);
-            console.log("Image URL");
-            console.log(url);
-          })
-    }else{
+
+      setCheckUploadStatus(true);
+    } else {
+      alert("Kindly fill remaining fields first");
+    }
+  }
+
+
+  const addNewProduct = async () => {
+    console.log("inside");
+
+    if (id && name && category && amount && imagePath) {
+      if(checkUploadStatus){
+        productAdd(id, name, category, amount, imageURL).then(() => {
+          alert('Product Added Successfully');
+          setId("");
+          setAmount("");
+          setCategory("");
+          document.getElementById("admin-add-input").value="";
+          setName("");
+        });
+      }else{
+        alert("Click on \"Upload Image\", before adding the product ");
+      }
+    }
+    else {
       alert("Kindly fill remaining fields first");
     }
   }
 
 
 
-  const addNewProduct = () => {
-    console.log("inside");
-    productAdd(id, name, category, amount, imageURL).then(()=> {
-      alert('Product Added Successfully');
-    });
+  const deleteProduct = (product_id) => {
+    if(window.confirm(`Delete product of id '${product_id}' ?`)){
+      productDelete(product_id).then((res)=>{
+        alert("Product Deleted");
+      });
+    }
   }
 
 
@@ -82,6 +108,8 @@ const Admin = () => {
                   <th id="admin-th-head">Image</th>
                   <th id="admin-th-head">Category</th>
                   <th id="admin-th-head">Amount</th>
+                  <th id="admin-th-head">Update</th>
+                  <th id="admin-th-head">Delete</th>
                 </tr>
                 {
                   productList && productList.map((data, key) => {
@@ -89,9 +117,11 @@ const Admin = () => {
                       <tr id="admin-tr" key={key}>
                         <td id="admin-td">{data.id}</td>
                         <td id="admin-td">{data.product_name}</td>
-                        <td id="admin-td"><img id="admin-td-img" src={data.product_image} alt={data.product_image} /></td>
+                        <td id="admin-td admin-td-size"><img id="admin-td-img" src={data.product_image} alt={data.product_image} /> <a href={data.product_image} target="_blank"><button>View Image</button></a></td>
                         <td id="admin-td">{data.product_category}</td>
                         <td id="admin-td">{data.product_amount}</td>
+                        <td id="admin-td"><button style={{backgroundColor:'grey', color:'#fff', padding:'5px', border:'none', cursor:'pointer'}}>Update</button></td>
+                        <td id="admin-td"><button onClick={()=> deleteProduct(data.id)} style={{backgroundColor:'red', color:'#fff', padding:'5px', border:'none', cursor:'pointer'}}>Delete</button></td>
                       </tr>
                     )
                   })
@@ -100,15 +130,18 @@ const Admin = () => {
             </div>
           </div>
           <div id="admin-add-product">
-                <h3 id='admin-add-h3'>Upload New Products</h3>
-                <div id="admin-add-form">
-                  <input id="admin-add-input" onChange={(e) => setId(e.target.value)} placeholder='Product ID (001)' type="text" />
-                  <input id="admin-add-input" onChange={(e) => setName(e.target.value)} placeholder='Product Name' type="text" />
-                  <input id="admin-add-input" onChange={(e) => { uploadImagehandle(); setImagePath(e.target.files[0])}} type="file" />
-                  <input id="admin-add-input" onChange={(e) => setCategory(e.target.value)} placeholder='Product Category (laptop)' type="text" />
-                  <input id="admin-add-input" onChange={(e) => setAmount(e.target.value)} placeholder='Product Amount' type="text" />
-                  <button id="admin-add-btn" onClick={addNewProduct} >Add Product</button>
-                </div>
+            <h3 id='admin-add-h3'>Upload New Products</h3>
+            <div id="admin-add-form">
+              <input id="admin-add-input" value={id} onChange={(e) => setId(e.target.value)} placeholder='Product ID (001)' type="text" />
+              <input id="admin-add-input" value={name} onChange={(e) => setName(e.target.value)} placeholder='Product Name' type="text" />
+              <div style={{ display: "flex", alignItems: 'center' }}>
+                <input id="admin-add-input" onChange={(e) => { setImagePath(e.target.files[0]) }} type="file" />
+                <button id="admin-add-btn" style={{ fontSize: 10, height: 'fit-content', padding: '5px', backgroundColor: '#E384FF', color: '#fff', border: 'none', cursor: 'pointer' }} onClick={uploadImagehandle}>Upload Image</button>
+              </div>
+              <input id="admin-add-input" value={category} onChange={(e) => setCategory(e.target.value)} placeholder='Product Category (laptop)' type="text" />
+              <input id="admin-add-input" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Product Amount' type="text" />
+              <button id="admin-add-btn" onClick={addNewProduct} >Add Product</button>
+            </div>
           </div>
         </div>
       </div>
